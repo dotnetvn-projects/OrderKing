5677246e4774a4e6876d12c2c150e86268e98652
Trung <trungle.it@gmail.com>
2019-01-22 21:05:03 +0700

    Add auth modules

--- Source/OrderKing.Sell/src/app/presentation/main/auth/auth.module.ts
+++ Source/OrderKing.Sell/src/app/presentation/main/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { AuthComponent } from './auth.component';
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 const authRoutes: Routes = [
