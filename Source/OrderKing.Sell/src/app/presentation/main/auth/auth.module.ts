4e5f6c5451c2d310845b564b2f93cd0d01f78af0
Trung <trungle.it@gmail.com>
2019-02-13 23:25:20 +0700

    Unapply lazy load for LoginComponent on auth module

--- Source/OrderKing.Sell/src/app/presentation/main/auth/auth.module.ts
+++ Source/OrderKing.Sell/src/app/presentation/main/auth/auth.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { AuthComponent } from './auth.component';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
 
 const authRoutes: Routes = [
   {
     path: '',
     component: AuthComponent,
     children: [
-      { path: 'login', loadChildren: './login/login.module#LoginModule' },
+      { path: 'login', component: LoginComponent },
     ]
   }
 ];
